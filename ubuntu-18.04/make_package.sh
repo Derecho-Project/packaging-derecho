3096b03c3b73b1f65ae74ed337527228aab3e94f
Matthew Milano <milano@cs.cornell.edu>
2019-10-09 18:20:39 -0400

    hopefully making the script more robust

--- ubuntu-18.04/make_package.sh
+++ ubuntu-18.04/make_package.sh
@@ -8,49 +8,26 @@ set -eu
 
 (
     set -eu
-    tmpdir=""
-    
-    function cleanup {
-	if [[ -d $tmpdir ]]; then 
-	    echo "Removing $tmpdir"
-	    #rm  -r /tmp/foo
-	fi
-    }
-    
-    trap cleanup EXIT
+  
+    source make_package_common.sh
 
     if [[ $# -gt 0 ]]; then cd $1; fi 
 
-    if [[ -d debian && -d work && `ls | wc -w` -eq 2 ]]; then
-	echo "Working directory `pwd` detected as compliant"
-	original_dir=`pwd`
-	current_dir_name=`pwd | rev | cut -d/  -f1 | rev`
-	if echo $current_dir_name | grep -q packaging-; then
-	    project_name=`echo $current_dir_name | sed s/"packaging-"//g`
-	    debian_dir=`pwd`/debian
-	    tmpdir=`mktemp -d`
-	    cp -rfp work/* $tmpdir
-	 cd $tmpdir
-	 if [[ `echo $project_name*/ | wc -w` -eq 1 ]]; then 
-	     package_dir=`echo $project_name*/ | tr -d /`
-	     tar -czf "$package_dir".tar.gz $package_dir
-	     cd $package_dir
-	     debmake
-	     yes | cp -r $debian_dir/* debian/
-	     debuild
-	     cp ../*.deb $original_dir
-	     exit 0
-	 else
-	     echo "Error: more than one directory in this package's work directory.  Candidates are: `echo $project_name*/`"
-	 fi
-	else
-	    echo 'Error: directory does not follow "package-FOO" name format'
-	fi
+    ensure_dir_correct
+    if [[ $? -eq 0 ]]; then
+	enter_tmpdir
+	cp -rfp $workdir_full/* $tmpdir_full
+	tar -czf "$package_dir_relative".tar.gz $package_dir_relative
+	cd $package_dir_relative
+	debmake
+	yes | cp -r $debian_dir_full/* debian/
+	debuild
+	cp ../*.deb $original_dir_full
+	exit 0
     else
-	echo "Error: ill-formatted working directory.  Cannot find debian and work directories, or extra items present.  Remove the .deb file, probably."
+	exit 1
     fi
-
-    exit 1
+    
 )
 
 exit $?
---
+++ ubuntu-18.04/make_package_common.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+tmpdir_full=""
+function cleanup {
+    if [[ -d $tmpdir_full ]]; then 
+	echo "Removing $tmpdir_full"
+	#rm  -r /tmp/foo
+    fi
+}
+
+trap cleanup EXIT
+
+function ensure_dir_correct {
+    if [[ -d debian && -d work && `ls | wc -w` -eq 2 ]]; then
+	echo "Working directory `pwd` detected as compliant"
+	local current_dir_name=`pwd | rev | cut -d/  -f1 | rev`
+	if echo $current_dir_name | grep -q packaging-; then
+	    local project_name=`echo $current_dir_name | sed s/"packaging-"//g`
+	    if [[ `echo work/$project_name*/ | wc -w` -eq 1 ]]; then
+		original_dir_full=`pwd`
+		current_dir_name=`pwd | rev | cut -d/  -f1 | rev`
+		project_name=`echo $current_dir_name | sed s/"packaging-"//g`
+		package_dir_relative=`echo work/$project_name*/ | sed s/work// | tr -d /`
+		debian_dir_full=`pwd`/debian
+		workdir_full=`pwd`/work
+		return 0
+	    else
+		echo "Error: more than one directory in this package's work directory.  Candidates are: `echo $project_name*/`"
+	    fi
+	else
+	    echo 'Error: directory does not follow "package-FOO" name format'
+	fi
+    else
+	echo "Error: ill-formatted working directory.  Cannot find debian and work directories, or extra items present.  Remove the .deb file, probably."
+    fi
+    return 1
+}
+
+function enter_tmpdir {
+    tmpdir_full=`mktemp -d`
+    cd $tmpdir_full
+    echo $tmpdir_full
+}
